709adc0a35d744a7cf67a746e4b6fa1109683e02
Sanyok-top <sasha.tsakhlo@gmail.com>
2021-08-04 17:02:47 +0300

    hw_18

---
+++ js1/js/main.js
@@ -0,0 +1,36 @@
+"use strict"
+
+let blockContacts = 10;
+console.log(blockContacts);
+if (blockContacts <= 4){
+	console.log(blockContacts);
+}
+else{
+	console.log('Неверно');
+}
+blockContacts = String(blockContacts);
+console.log(blockContacts);
+let userInfo = {
+	name: 'Alexandr',
+	age:41,
+	location: 'Trebukhov'
+}
+console.log(userInfo);
+let number = 12;
+console.log(number)
+let copi = `Hello ${userInfo}`;
+console.log(copi);
+console.log('35' + - "22");
+console.log(35 + (- 22));
+console.log('35' * "22");
+let num = 22;
+console.log('558' > num++);
+console.log('558' > 22);
+console.log('558' > 22);
+
+if (0) {
+	console.log('ложь');
+}else if(" ") {
+	console.log('истина');
+}
+
